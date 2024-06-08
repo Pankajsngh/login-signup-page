72b3958f458974f3acd2bb483e0c15a5f3dcb243
Pankaj Singh <pankajsingh@Pankajs-MacBook-Pro.local>
2024-06-08 23:46:39 +0100

    Initial commit with login and sign-up pages

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+document.getElementById('loginForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Logged in!');
+});
+
+document.getElementById('signupForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Signed up!');
+});
